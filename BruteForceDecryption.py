7f2565fb6a52b7a7b1d89ffb7737d8f339c3d044
ThatGoblinKing <68427291+ThatGoblinKing@users.noreply.github.com>
2025-02-05 13:40:05 -0700

    Added Read From File + Threading

--- BruteForceDecryption.py
+++ BruteForceDecryption.py
@@ -1,9 +1,13 @@
 import enchant
-from threading import Thread
+from threading import Thread, Lock
 from string import ascii_lowercase, ascii_uppercase
+from datetime import datetime
 
-MAX_THREADS = 5
+FILE_PATH = 'decrypt.txt'
+MAX_THREADS = 26
+lock = Lock()
 
+correct_tally = {i:0 for i in range(26)}
 d = enchant.Dict("en_US")
 
 def caesar_decrypt(key, encrypted_string):
@@ -19,39 +23,38 @@ def caesar_decrypt(key, encrypted_string):
             result += char
     return result
 
-
-def check_keys(min: int, max: int, correct_tally):
+def check_keys(min: int, max: int):
+    global correct_tally
     for i in range(min, max):
         decrypted_string = caesar_decrypt(i, message)
         words = decrypted_string.split(" ")
 
         for word in words:
             # print(word, d.check(word))
-            if d.check(word) is True:
+            if d.check(word):
                 #print(f"Detected Word: {word}\nKey: {i}")
-                correct_tally[i] += 1
+                with lock:
+                    correct_tally[i] += 1
             else:
                 pass
 
-correct_tally = {i:0 for i in range(26)}
-
-message = input("Enter caesar obfuscated string: ")
+step = 26 // MAX_THREADS
+with open(FILE_PATH, 'r') as file:
+    message = file.read()
 threads = [None] * MAX_THREADS
+startTime = datetime.now()
 for i in range(MAX_THREADS):
-    min = int(i/MAX_THREADS)
-    max = int((i + 1)/MAX_THREADS)
-    threads[i] = Thread(target=check_keys, args=(min, max, correct_tally))
+    min = int(step * i)
+    max_val = (i + 1) * step if i != MAX_THREADS - 1 else 26
+    threads[i] = Thread(target=check_keys, args=(min, max_val))
     threads[i].start()
 
 for i in range(len(threads)):
     threads[i].join()
 
+time_taken = datetime.now() - startTime
 
-
-most_likely_key = max(d, key=lambda k: d[k])
-print("Found key:", most_likely_key)
-print("Decrypted Message:", caesar_decrypt(most_likely_key, message))
-    
-            # possible_decrypted.append([caesar_decrypt(i, z) for z in words])
-    
-# print(possible_decrypted)
+most_likely_key = max(correct_tally, key=correct_tally.get)
+print(f"""Found key: {most_likely_key}
+Decrypted Message: {caesar_decrypt(most_likely_key, message)}
+Time Taken: {time_taken.seconds}s {time_taken.microseconds / 1000}ms""")
